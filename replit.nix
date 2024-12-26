e85c836853cc2602d60d7dc546e489b41942a9ed
Harsh Jha <jhaharsh29@gmail.com>
2024-12-26 23:04:45 +0530

    Add files via upload

---
+++ replit.nix
@@ -0,0 +1,23 @@
+{pkgs}: {
+  deps = [
+    pkgs.pkg-config
+    pkgs.openssl
+    pkgs.c-ares
+    pkgs.geos
+    pkgs.tesseract
+    pkgs.cacert
+    pkgs.zlib
+    pkgs.tk
+    pkgs.tcl
+    pkgs.openjpeg
+    pkgs.libxcrypt
+    pkgs.libwebp
+    pkgs.libtiff
+    pkgs.libjpeg
+    pkgs.libimagequant
+    pkgs.lcms2
+    pkgs.freetype
+    pkgs.grpc
+    pkgs.glibcLocales
+  ];
+}
